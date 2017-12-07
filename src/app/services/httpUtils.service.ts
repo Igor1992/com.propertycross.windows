859f2350f8c6045455ca3af4edacfe00efaef76e
Igor1992 <igor.khamianok@mail.ru>
2017-12-07 18:02:10 +0300

    new service httpUtils

--- src/app/services/httpUtils.service.ts
+++ src/app/services/httpUtils.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class HttpUtils {
-  getParams(paramsObj): string {
+  getParams(paramsObj: any): string {
     return Object.keys(paramsObj)
       .map(key => `${key}=${paramsObj[key]}`)
       .join('&');
