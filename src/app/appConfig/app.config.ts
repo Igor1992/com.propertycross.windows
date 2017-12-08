e32a9c818b98dc1b7fbfa9936821c3dbbf14595c
Igor1992 <igor.khamianok@mail.ru>
2017-12-08 17:40:26 +0300

    refactoring path httpUtils service

--- src/app/appConfig/app.config.ts
+++ src/app/appConfig/app.config.ts
@@ -5,11 +5,13 @@ export const COUNTRY_NAME_KEY = 'countryName';
 export const ERROR_CHOOSE_COUNTRY = "Please choose country!";
 
 export const NUM_START_PAGE = 1;
-export const AUTO_STR_SEARCH_VALUES = ["Paris", "London", "Leeds", "Berlin", "Madrid", "Rome", "Barcelona", "Toulouse",
-  "Strasbourg", "Dijon", "Grenoble", "Aix-en-Provence", "Caen", "Nimes", "Zaragoza", "Wales"];
+export const AUTO_STR_SEARCH_VALUES = ["Paris","Anjangaon", "London", "Leeds", "Berlin", "Madrid", "Rome", "Barcelona",
+  "Toulouse", "Strasbourg", "Dijon", "Grenoble", "Aix-en-Provence", "Caen", "Nimes", "Zaragoza", "Wales", "Barh",
+  "Dhubri", "Gangotri", "Hansi", "Itarsi", "Jagtial", "Kadi", "Lakhisarai", "Macherla", "Nalbari", "Pacode", "Qadian",
+  "Rahatgarh"];
 
 export const COUNTRIES: any[] = [
-  {name:"br",value: "com.br"}, {name:"Германия",value:"de"},
-  {name:"Испания",value:"es"}, {name:"Франция",value:"fr"}, {name:"in",value:"in"},
-  {name:"Италия",value:"it"}, {name:"mx",value:"mx"}, {name:"uk",value:"co.uk"}
+  {name:"Бразилия",value: "com.br"}, {name:"Германия",value:"de"},
+  {name:"Испания",value:"es"}, {name:"Франция",value:"fr"}, {name:"Индия",value:"in"},
+  {name:"Италия",value:"it"}, {name:"Мексика",value:"mx"}, {name:"Великобритания",value:"co.uk"}
 ];
